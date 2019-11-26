ba934b37a19fa0d3625d5f1d805d973f49d75789
liach <liach@users.noreply.github.com>
2019-11-25 21:49:46 -0600

    这补丁要死人了 赶快转战Mixin吧
    Signed-off-by: liach <liach@users.noreply.github.com>

--- src/main/java/snownee/nimble/asm/NimbleLaunchPlugin.java
+++ src/main/java/snownee/nimble/asm/NimbleLaunchPlugin.java
@@ -1,5 +1,6 @@
 package snownee.nimble.asm;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Set;
 import java.util.function.BiFunction;
@@ -7,14 +8,14 @@ import java.util.function.BiFunction;
 import javax.annotation.Nonnull;
 
 import net.thesilkminer.mc.fermion.asm.api.LaunchPlugin;
+import net.thesilkminer.mc.fermion.asm.api.MappingUtilities;
 import net.thesilkminer.mc.fermion.asm.api.PluginMetadata.Builder;
 import net.thesilkminer.mc.fermion.asm.api.descriptor.ClassDescriptor;
+import net.thesilkminer.mc.fermion.asm.api.descriptor.MethodDescriptor;
 import net.thesilkminer.mc.fermion.asm.api.transformer.TransformerData;
 import net.thesilkminer.mc.fermion.asm.prefab.AbstractLaunchPlugin;
-import net.thesilkminer.mc.fermion.asm.prefab.AbstractTransformer;
+import net.thesilkminer.mc.fermion.asm.prefab.transformer.SingleTargetMethodTransformer;
 
-import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
@@ -44,114 +45,96 @@ public final class NimbleLaunchPlugin extends AbstractLaunchPlugin
         return ROOT_PACKAGES;
     }
 
-    static final class CameraSetupTransformer extends AbstractTransformer
+    static final class CameraSetupTransformer extends SingleTargetMethodTransformer
     {
 
         CameraSetupTransformer(@Nonnull LaunchPlugin plugin)
         {
-            super(TransformerData.Builder.create()
+            super(TransformerData.Builder
+                            .create()
                             .setOwningPlugin(plugin)
                             .setName("Camera Setup Event Transformer")
-                            .setDescription("Why the heck do I need a description")
+                            .setDescription("Post camera setup events")
                             .build(),
-                    ClassDescriptor.of("net.minecraft.client.renderer.ActiveRenderInfo")
+                    ClassDescriptor.of("net.minecraft.client.renderer.ActiveRenderInfo"),
+                    MethodDescriptor.of("func_216772_a", Arrays.asList(
+                            ClassDescriptor.of("net.minecraft.world.IBlockReader"),
+                            ClassDescriptor.of("net.minecraft.entity.Entity"),
+                            ClassDescriptor.of(boolean.class),
+                            ClassDescriptor.of(boolean.class),
+                            ClassDescriptor.of(float.class)
+                    ), ClassDescriptor.of(void.class))
             );
         }
 
-        @Nonnull
-        @Override
-        public BiFunction<Integer, ClassVisitor, ClassVisitor> getClassVisitorCreator()
+        @Nonnull @Override protected BiFunction<Integer, MethodVisitor, MethodVisitor> getMethodVisitorCreator()
         {
-            return (version, parent) -> new ClassVisitor(version, parent)
+            return (api, parent) -> new MethodVisitor(api, parent)
             {
+                boolean shouldInject = true;
                 @Override
-                public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions)
+                public void visitMethodInsn(int opcode, String owner, String name, String descriptor, boolean isInterface)
                 {
-                    MethodVisitor parent = super.visitMethod(access, name, descriptor, signature, exceptions);
-                    boolean mcp = name.equals("update");
-                    if (mcp || name.equals("func_216772_a"))
-                    {
-                        return new MethodVisitor(api, parent)
-                        {
-                            boolean line57Visited = false;
-                            @Override 
-                            public void visitFrame(int type, int nLocal, Object[] local, int nStack, Object[] stack)
-                            {
-                                super.visitFrame(type, nLocal, local, nStack, stack);
-                                if (line57Visited && type == Opcodes.F_CHOP) {
-                                    injectCode();
-                                }
-                            }
-
-                            @Override
-                            public void visitMaxs(int maxStack, int maxLocals)
-                            {
-                                super.visitMaxs(Math.max(maxStack, 4), Math.max(maxLocals, 6));
-                            }
-
-                            @Override
-                            public void visitLineNumber(int line, Label start)
-                            {
-                                super.visitLineNumber(line, start);
-                                if (line == 57)
-                                {
-                                    line57Visited = true; // maybe use a different logic in the future
-                                }
-                            }
-
-                            private void injectCode()
-                            {
-                                final String pitchFieldName = mcp ? "pitch" : "field_216797_i";
-                                final String yawFieldName = mcp ? "yaw" : "field_216798_j";
-
-                                // this
-                                super.visitVarInsn(Opcodes.ALOAD, 0);
-                                // partial
-                                super.visitVarInsn(Opcodes.AALOAD, 5);
-                                // this.pitch
-                                super.visitVarInsn(Opcodes.ALOAD, 0);
-                                super.visitFieldInsn(Opcodes.GETFIELD, "net/minecraft/client/renderer/ActiveRenderInfo", pitchFieldName, "F");
-                                // this.yaw
-                                super.visitVarInsn(Opcodes.ALOAD, 0);
-                                super.visitFieldInsn(Opcodes.GETFIELD, "net/minecraft/client/renderer/ActiveRenderInfo", yawFieldName, "F");
-                                // Nimble.fireCameraSetupEvent; use "copy mixin target reference" in minecraft dev intellij plugin
-                                // Lsnownee/nimble/Nimble;fireCameraSetupEvent(Lnet/minecraft/client/renderer/ActiveRenderInfo;FFF)Lnet/minecraftforge/client/event/EntityViewRenderEvent$CameraSetup;
-                                super.visitMethodInsn(Opcodes.INVOKESTATIC, "snownee/nimble/Nimble", "fireCameraSetupEvent",
-                                        "(Lnet/minecraft/client/renderer/ActiveRenderInfo;FFF)Lnet/minecraftforge/client/event/EntityViewRenderEvent$CameraSetup;",
-                                        false);
-
-                                final int eventVarIndex = 6;
-                                // dup
-                                super.visitVarInsn(Opcodes.ASTORE, eventVarIndex);
-
-                                // Lnet/minecraftforge/client/event/EntityViewRenderEvent$CameraSetup;getYaw()F
-                                super.visitVarInsn(Opcodes.ALOAD, eventVarIndex);
-                                super.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "net/minecraftforge/client/event/EntityViewRenderEvent$CameraSetup", "getYaw",
-                                        "()F", false);
-                                // this.yaw = 
-                                super.visitFieldInsn(Opcodes.PUTFIELD, "net/minecraft/client/renderer/ActiveRenderInfo", yawFieldName, "F");
-
-                                // Lnet/minecraftforge/client/event/EntityViewRenderEvent$CameraSetup;getPitch()F
-                                super.visitVarInsn(Opcodes.ALOAD, eventVarIndex);
-                                super.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "net/minecraftforge/client/event/EntityViewRenderEvent$CameraSetup", "getPitch",
-                                        "()F", false);
-                                // this.pitch = 
-                                super.visitFieldInsn(Opcodes.PUTFIELD, "net/minecraft/client/renderer/ActiveRenderInfo", pitchFieldName, "F");
-
-                                //GlStateManager.rotatef(roll, 0, 0, 1);
-                                // Lnet/minecraftforge/client/event/EntityViewRenderEvent$CameraSetup;getRoll()F
-                                super.visitVarInsn(Opcodes.ALOAD, eventVarIndex);
-                                super.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "net/minecraftforge/client/event/EntityViewRenderEvent$CameraSetup", "getRoll",
-                                        "()F", false);
-                                super.visitInsn(Opcodes.DCONST_0);
-                                super.visitInsn(Opcodes.DCONST_0);
-                                super.visitInsn(Opcodes.DCONST_1);
-                                // Lcom/mojang/blaze3d/platform/GlStateManager;rotatef(FFFF)V
-                                super.visitMethodInsn(Opcodes.INVOKESTATIC, "com/mojang/blaze3d/platform/GlStateManager", "rotatef", "(FFFF)V", false);
-                            }
-                        };
+                    // Lcom/mojang/blaze3d/platform/GlStateManager;rotatef(FFFF)V
+                    if (shouldInject && opcode == Opcodes.INVOKESTATIC && owner.equals("com/mojang/blaze3d/platform/GlStateManager") && name.equals("rotatef") && descriptor.equals("(FFFF)V")) {
+                        // inject right here!
+                        injectCode();
+                        shouldInject = false;
                     }
-                    return parent;
+                    super.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
+                }
+
+                private void injectCode()
+                {
+                    final String pitchFieldName = MappingUtilities.INSTANCE.mapField("field_216797_i");
+                    final String yawFieldName = MappingUtilities.INSTANCE.mapField("field_216798_j");
+
+                    // this
+                    super.visitVarInsn(Opcodes.ALOAD, 0);
+                    // partial
+                    super.visitVarInsn(Opcodes.FLOAD, 5);
+                    // this.pitch
+                    super.visitVarInsn(Opcodes.ALOAD, 0);
+                    super.visitFieldInsn(Opcodes.GETFIELD, "net/minecraft/client/renderer/ActiveRenderInfo", pitchFieldName, "F");
+                    // this.yaw
+                    super.visitVarInsn(Opcodes.ALOAD, 0);
+                    super.visitFieldInsn(Opcodes.GETFIELD, "net/minecraft/client/renderer/ActiveRenderInfo", yawFieldName, "F");
+                    // Nimble.fireCameraSetupEvent; use "copy mixin target reference" in minecraft dev intellij plugin
+                    // Lsnownee/nimble/Nimble;fireCameraSetupEvent(Lnet/minecraft/client/renderer/ActiveRenderInfo;FFF)Lnet/minecraftforge/client/event/EntityViewRenderEvent$CameraSetup;
+                    super.visitMethodInsn(Opcodes.INVOKESTATIC, "snownee/nimble/Nimble", "fireCameraSetupEvent",
+                            "(Lnet/minecraft/client/renderer/ActiveRenderInfo;FFF)Lnet/minecraftforge/client/event/EntityViewRenderEvent$CameraSetup;",
+                            false);
+
+                    final int eventVarIndex = 6;
+                    // dup
+                    super.visitVarInsn(Opcodes.ASTORE, eventVarIndex);
+
+                    super.visitVarInsn(Opcodes.ALOAD, 0);
+                    // Lnet/minecraftforge/client/event/EntityViewRenderEvent$CameraSetup;getYaw()F
+                    super.visitVarInsn(Opcodes.ALOAD, eventVarIndex);
+                    super.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "net/minecraftforge/client/event/EntityViewRenderEvent$CameraSetup", "getYaw",
+                            "()F", false);
+                    // this.yaw =
+                    super.visitFieldInsn(Opcodes.PUTFIELD, "net/minecraft/client/renderer/ActiveRenderInfo", yawFieldName, "F");
+
+                    super.visitVarInsn(Opcodes.ALOAD, 0);
+                    // Lnet/minecraftforge/client/event/EntityViewRenderEvent$CameraSetup;getPitch()F
+                    super.visitVarInsn(Opcodes.ALOAD, eventVarIndex);
+                    super.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "net/minecraftforge/client/event/EntityViewRenderEvent$CameraSetup", "getPitch",
+                            "()F", false);
+                    // this.pitch =
+                    super.visitFieldInsn(Opcodes.PUTFIELD, "net/minecraft/client/renderer/ActiveRenderInfo", pitchFieldName, "F");
+
+                    //GlStateManager.rotatef(roll, 0, 0, 1);
+                    // Lnet/minecraftforge/client/event/EntityViewRenderEvent$CameraSetup;getRoll()F
+                    super.visitVarInsn(Opcodes.ALOAD, eventVarIndex);
+                    super.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "net/minecraftforge/client/event/EntityViewRenderEvent$CameraSetup", "getRoll",
+                            "()F", false);
+                    super.visitInsn(Opcodes.FCONST_0);
+                    super.visitInsn(Opcodes.FCONST_0);
+                    super.visitInsn(Opcodes.FCONST_1);
+                    // Lcom/mojang/blaze3d/platform/GlStateManager;rotatef(FFFF)V
+                    super.visitMethodInsn(Opcodes.INVOKESTATIC, "com/mojang/blaze3d/platform/GlStateManager", "rotatef", "(FFFF)V", false);
                 }
             };
         }
