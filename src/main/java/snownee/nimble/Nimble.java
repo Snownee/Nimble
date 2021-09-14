68624af56b6e693d548842bc9aa6ee7f58e96bde
Snownee <mjy523@126.com>
2021-09-14 20:23:54 +0800

    byebye ubuntu pastebin

--- src/main/java/snownee/nimble/Nimble.java
+++ src/main/java/snownee/nimble/Nimble.java
@@ -1,5 +1,7 @@
 package snownee.nimble;
 
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.forgespi.Environment;
@@ -10,6 +12,7 @@ public class Nimble {
 	public static final String NAME = "Nimble";
 
 	public Nimble() {
+		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "anything. i don't care", (remoteversionstring, networkbool) -> networkbool));
 		if (Environment.get().getDist().isClient()) {
 			FMLJavaModLoadingContext.get().getModEventBus().addListener(NimbleHandler::preInit);
 		}
