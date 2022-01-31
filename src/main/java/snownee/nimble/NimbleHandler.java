3d7ba7721fd4ec56703996998478404ddbd2ad21
Snownee <i@snownee.com>
2022-01-31 11:50:50 +0800

    fix: xray
    # Conflicts:
    #	src/main/java/snownee/nimble/NimbleHandler.java

--- src/main/java/snownee/nimble/NimbleHandler.java
+++ src/main/java/snownee/nimble/NimbleHandler.java
@@ -85,7 +85,7 @@ public class NimbleHandler {
 		if (distance < 1) {
 			float f = Mth.sin((float) (distance * Math.PI) / 2);
 			CameraAccess info = (CameraAccess) event.getCamera();
-			info.callMove(-info.callGetMaxZoom((f - 1) * 3), 0, 0);
+			info.callMove(info.callGetMaxZoom((1 - f) * 3), 0, 0);
 		}
 	}
 
