44da0db35fc51432b46d5887829918ff39637de7
Snownee <i@snownee.com>
2023-01-25 08:11:48 +0800

    feat: a more friendly way to enable disabled mixins

---
+++ src/main/java/snownee/nimble/mixin/MixinPlugin.java
@@ -0,0 +1,48 @@
+package snownee.nimble.mixin;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Set;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+public class MixinPlugin implements IMixinConfigPlugin {
+
+	@Override
+	public void onLoad(String mixinPackage) {
+	}
+
+	@Override
+	public String getRefMapperConfig() {
+		return null;
+	}
+
+	@Override
+	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+		return true;
+	}
+
+	@Override
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+	}
+
+	@Override
+	public List<String> getMixins() {
+		if (Files.exists(Paths.get("config", ".useNimbleModelFading"))) {
+			return List.of("ElytraLayerMixin", "HumanoidArmorLayerMixin", "LivingEntityRendererMixin", "ModelPartMixin");
+		}
+		return null;
+	}
+
+	@Override
+	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+	}
+
+	@Override
+	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+	}
+
+}
